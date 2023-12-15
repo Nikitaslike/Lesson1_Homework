1b18a32868b46a0026e45a31eca5102d18f92f1f
Nikitaslike <nikitaslike@gmail.com>
2023-12-15 09:05:27 +0200

    Test :0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int Order1 = 20;
+        System.out.println("Hi,Dear");
+        System.out.println(":)");
+        System.out.println("Did you order a Pizza «Marharita» and Suchi «Red Dragon»?");
+        System.out.println("If yes...");
+        System.out.println("You must to pay");
+        System.out.println(Order1+"¢");
+        System.out.println("Have a nice day!");
+        System.out.println("😉");
+    }
+}
